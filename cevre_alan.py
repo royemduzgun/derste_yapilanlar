749b090f774c73b10ee2a15f0c1f299cec9962f1
royem duzgun <154828792+royemduzgun@users.noreply.github.com>
2024-01-21 20:36:53 +0300

    Add files via upload

---
+++ cevre_alan.py
@@ -0,0 +1,6 @@
+kenar=float(input("lütfen ücgenin kenar uzunlugunu girin:"))
+yükseklik=float(input("lütfen bu kenara ait yuksekligi girin:"))
+
+alan=(kenar*yükseklik)/2
+print(f"ücgenin alanı:{alan}")
+    
