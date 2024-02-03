3025fde25c2a11371cd8f9454b62777a22505a9f
royem duzgun <154828792+royemduzgun@users.noreply.github.com>
2024-02-03 19:31:05 +0300

    Add files via upload

---
+++ dizi2.py
@@ -0,0 +1,10 @@
+meyveler=["elma","armut","kiraz"]
+print(meyveler)
+
+print(meyveler[1])
+print(meyveler[-1])
+
+print(len(meyveler))
+print("-------------")
+for a in range(len(meyveler)):
+    print(meyveler[a])
